bd3347a0222efb5da3365704ea8a85ebacf7bc41
William Bitencourt <williambokl@hotmail.com>
2021-10-19 01:18:10 -0300

    arquivos ajustes

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,28 @@
-function moverMenu(distMenu, distBody) {
-    $('.menu').animate({
-        left: distMenu + "px"
-    }, 200);
-
-    $('body').animate({
-        left: distBody + "px"
-    }, 200);
-}
-
-$(document).ready(() => {
-    $('.icon-menu').click(() => { moverMenu(0, 350) });
-    $('.icon-close').click(() => { moverMenu(-350, 0) });
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.collapsible').collapsible();
 });
+
+var fecharMenu = $('#menu-voador-open');
+var abrirMenu = $('#menu-voador-close');
+
+abrirMenu.click(function () {
+    fecharMenu.removeClass('display-none');
+    abrirMenu.addClass('display-none');
+    $('.menu-toponovo').addClass('margin-left-aberto');
+    $('.body-session').addClass('margin-left-aberto');
+    console.log('removi o atributo none do aberto');
+})
+
+fecharMenu.click(function () {
+    $('.sidenav').sidenav('close');
+    abrirMenu.removeClass('display-none');
+    fecharMenu.addClass('display-none');
+    $('.menu-toponovo').removeClass('margin-left-aberto');
+    $('.body-session').removeClass('margin-left-aberto')
+})
+
+var collapsibleElem = document.querySelector('.collapsible');
+var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+var elem = document.querySelectorAll('.sidenav');
+var instance = M.Sidenav.getInstance(elem);
